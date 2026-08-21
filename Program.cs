93ff56d0e3ac10ca7e9dd6af8d7caf2f77d25aa9
Arturito <quezadaalturo@gmail.com>
2026-08-21 18:58:54 -0400

    Crear portada pública premium de TropiNails

--- Program.cs
+++ Program.cs
@@ -241,7 +241,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Login}/{id?}");
+   pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // HUBS
 app.MapHub<OnlineHub>("/onlineHub");
